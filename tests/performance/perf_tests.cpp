ac5ebfc3c8594c4118c49de935a58f3ea59603bb
Martin Noring <martin.noring@gmail.com>
2022-03-19 21:38:33 +0100

    train VSOM performance test

--- tests/performance/perf_tests.cpp
+++ tests/performance/perf_tests.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include <fstream>
 
-int verbose = 0;
+int verbose = 1;
 
 namespace Perftests
 {
@@ -35,11 +35,36 @@ namespace Perftests
         void teardown();
 
     public:
-        SomTests() :
-            sut{1, 1, 1},
-            numberOfRuns{100} 
-        {}
+        SomTests() : sut{1, 1, 1},
+                     numberOfRuns{100}
+        {
+        }
 
+        auto test_train(char *dbPath, const char *columnSpecPath, int weightDecayFunction)
+        {
+            DataBase db;
+            auto dbOpenResult = db.open(dbPath);
+            assert(dbOpenResult != 0);
+
+            DataSet trainingSet(columnSpecPath);
+
+            trainingSet.loadDataBase(&db);
+
+            sut = Som{100, 100, trainingSet.vectorLength()};
+
+            sut.randomInitialize(std::time(NULL), 1);
+
+            auto numOfEpochs = int{100};
+            auto eta0 = double{0.01};
+            auto etaDec = double{0.01};
+            auto sigma0 = double{20.0};
+            auto sigmaDec = double{0.01};
+
+            auto startTime = high_resolution_clock::now();
+            sut.train(&trainingSet, numOfEpochs, eta0, etaDec, sigma0, sigmaDec, weightDecayFunction);
+            auto endTime = high_resolution_clock::now();
+            return (endTime - startTime) / numOfEpochs;
+        }
         auto test_randomInitialize()
         {
             setup();
@@ -80,5 +105,8 @@ int main()
     auto tester = SomTests{};
 
     printResultToFile(tester.test_randomInitialize(), "Som::randomInitialize()");
+    printResultToFile(tester.test_train("./data/testDb.sq3", "./data/columnSpec.txt", 0), "Som::train(exponentialWeightDecay)");
+    printResultToFile(tester.test_train("./data/testDb.sq3", "./data/columnSpec.txt", 1), "Som::train(inverseProportionalWeightDecay)");
+
     printResultToFile(tester.test_findBmu(), "Som::findBmu()");
 }
