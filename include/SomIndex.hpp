9732015a5512b624fe9853b58cae4e1e402a712b
Martin Noring <martin.noring@gmail.com>
2023-04-24 21:02:54 +0200

    extended SomIndex API with operator==

--- include/SomIndex.hpp
+++ include/SomIndex.hpp
@@ -6,16 +6,22 @@ class Som;
 
 class SomIndex
 {
-	protected:
-		size_t x,y;
-	
-	public:
-		SomIndex(size_t x, size_t y) noexcept;
-		SomIndex(const Som &map, size_t index) noexcept;
-		~SomIndex() = default;
-		size_t getSomIndex(const Som &som);
-		size_t getX() const noexcept;
-		size_t getY() const noexcept;
-		void setX(size_t index) noexcept;
-		void setY(size_t index) noexcept;
+protected:
+	size_t x, y;
+
+public:
+	SomIndex(size_t x, size_t y) noexcept;
+	SomIndex(const Som &map, size_t index) noexcept;
+	~SomIndex() = default;
+	size_t getSomIndex(const Som &som);
+	size_t getX() const noexcept;
+	size_t getY() const noexcept;
+	void setX(size_t index) noexcept;
+	void setY(size_t index) noexcept;
+
+	bool operator==(const SomIndex &other)
+	{
+		return x == other.x &&
+			y == other.y;
+	}
 };
