6ba241fd7c85b101db356b8e0bcf8dcbd6059c6c
Martin Noring <martin.noring@gmail.com>
2024-04-08 23:02:41 +0200

    combinatorial linear regression as template specialization

--- include/Transformation.hpp
+++ include/Transformation.hpp
@@ -1,29 +1,48 @@
 #pragma once
 
+#include "Eigen/Dense"
 #include <functional>
 
 #include <string>
+#include <sstream>
 
-template<typename T>
+template <typename T>
 struct Transformation
 {
     /* Standard transform by default TODO: Use dispersion and weight */
-    std::function<T(const T& value, const T& model, const T& dispersion, const T& valueWeight)> Comparer = 
-        [](const T &value, const T &model, const T &dispersion, const T &valueWeight) { return model - value; };
+    std::function<T(const T &value, const T &model, const T &dispersion, const T &valueWeight)> Comparer =
+        [](const T &value, const T &model, const T &dispersion, const T &valueWeight)
+    { return model - value; };
 
-    std::function<T(const T& value, const T& model, const T& valueWeight)> Stepper = 
-        [](const T &value, const T &model, const T& valueWeight) { return value - model;  };
+    std::function<T(const T &value, const T &model, const T &valueWeight)> Stepper =
+        [](const T &value, const T &model, const T &valueWeight)
+    { return value - model; };
 
-    std::function<T(const T& model)> Displayer = 
-        [](const T &model) { return model; };
+    std::vector<std::string> names = std::vector<std::string>{};
+    std::function<std::vector<std::string>(const T &model)> Displayer =
+        [&names = names](const T &model)
+    { return names; };
 
     std::string Name = std::string{"Standard transformation"};
 
-    static Transformation<const Eigen::VectorXf> CombinatorialLinearRegression()
+    static Transformation<T> CombinatorialLinearRegression(const std::vector<std::string> &columnNames);
+};
+
+template <>
+struct Transformation<const Eigen::VectorXf>
+{
+    
+    std::function<const Eigen::VectorXf(const Eigen::VectorXf &value, const Eigen::VectorXf &model, const Eigen::VectorXf &dispersion, const Eigen::VectorXf &valueWeight)> Comparer;
+    std::function<const Eigen::VectorXf(const Eigen::VectorXf &value, const Eigen::VectorXf &model, const Eigen::VectorXf &valueWeight)> Stepper;
+    std::vector<std::string> names = std::vector<std::string>{};
+    std::function<std::vector<std::string>(const Eigen::VectorXf &model)> Displayer;
+
+    std::string Name = std::string{"Linear regression"};
+    static Transformation<const Eigen::VectorXf> CombinatorialLinearRegression(const std::vector<std::string> &columnNames)
     {
         return Transformation<const Eigen::VectorXf>{
-                .Comparer = [](const Eigen::VectorXf &value, const Eigen::VectorXf &model, const Eigen::VectorXf &dispersion, const Eigen::VectorXf &valueWeight)
-                { 
+            .Comparer = [](const Eigen::VectorXf &value, const Eigen::VectorXf &model, const Eigen::VectorXf &dispersion, const Eigen::VectorXf &valueWeight)
+            { 
                     /* The model vector is split into two parts, A and B, where A*x' + B = y'
                      * x' and y' together constitutes all combinations of the elements of 'value',
                      * not considering direction of the combination with itself (so only the upper half matrix) */
@@ -83,10 +102,22 @@ struct Transformation
                 delta << aDelta , bDelta;
 
                 return delta; },
-            .Displayer = [](const Eigen::VectorXf &model) 
+            .Displayer = [columnNames](const Eigen::VectorXf &model)
             { 
-                return model; 
-            },
+                auto disp = std::vector<std::string>{};
+                disp.reserve(columnNames.size());
+
+                int index = 0;
+                for (int i = 0; i < model.size(); ++i) {
+                    for (int j = i + 1; j < model.size(); ++j) {
+                        std::stringstream ss;
+                        ss << columnNames[i] << " = " << model[index] << '*' << columnNames[j];
+
+                        disp.emplace_back(ss.str());
+                        index++;
+                    }
+                }
+                return disp; },
             .Name = "Linear regression"};
     }
 };
